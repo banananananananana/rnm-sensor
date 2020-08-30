9e400b5cc4b9a2f239d14fcf0466d58d74d99fd3
Thomas Kjær <thomas@thomaskjaer.com>
2020-08-30 10:48:06 +0200

    update /etc/hosts with new hostname

--- set-mac-as-hostname.sh
+++ set-mac-as-hostname.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-hostnamectl set-hostname "$(cat /sys/class/net/`ip route|grep default|sed -E 's/.*dev (\S*).*/\1/'`/address|sed 's/:/-/g')"
+HOSTNAME=$(cat /sys/class/net/`ip route|grep default|sed -E 's/.*dev (\S*).*/\1/'`/address|sed 's/:/-/g')
+perl -pi -e "s/127.0.0.1.*/127.0.0.1 localhost $HOSTNAME/" /etc/hosts
+hostnamectl set-hostname $HOSTNAME
