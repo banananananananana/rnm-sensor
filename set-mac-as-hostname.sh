b4e10f0b3aee0338f50716b09df53d1d549ce908
Thomas Kjær <thomas@thomaskjaer.com>
2020-08-30 10:33:05 +0200

    check for default device instead of assuming eth0

--- set-mac-as-hostname.sh
+++ set-mac-as-hostname.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-hostnamectl set-hostname "$(sed s/:/-/g </sys/class/net/eth0/address)"
+hostnamectl set-hostname "$(cat /sys/class/net/`ip route|grep default|sed -E 's/.*dev (\S*).*/\1/'`/address|sed 's/:/-/g')"
